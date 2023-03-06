301b677f352ac8b88bce39644d4736225ef7311c
Gadivalr <levi.sojos7@gmail.com>
2023-03-06 17:18:24 -0300

    version2

--- js/main.js
+++ js/main.js
@@ -8,7 +8,48 @@ $(function(){
         $('.main-menu-content').removeClass('active');
     })
 });
-    
+
+gsap.to("#bg",{
+    scrollTrigger:{
+        scrub:1
+    },
+    scale:1.5
+})
+gsap.to("#man",{
+    scrollTrigger:{
+        scrub:1
+    },
+    scale:0.5
+})
+gsap.to("#clouds_1",{
+    scrollTrigger:{
+        scrub:1
+    },
+    x:200
+})
+gsap.to("#clouds_2",{
+    scrollTrigger:{
+        scrub:1
+    },
+    x:-200
+})
+gsap.to("#text",{
+    scrollTrigger:{
+        scrub:1
+    },
+    y:500
+})
+// const cardTitles = document.querySelectorAll('.card-title');
+
+// cardTitles.forEach(title => {
+//   title.addEventListener('click', () => {
+//     window.open('ejemplo.cl', '_blank');
+//   });
+// });
+function openPage(pageName) {
+    window.location.href = pageName;
+  }
+
     
     
     
